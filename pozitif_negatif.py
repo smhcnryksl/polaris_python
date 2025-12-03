95d8699b7a0342a72c9107b557d368359048ca13
smhcnryksl <142000426+smhcnryksl@users.noreply.github.com>
2025-12-03 19:40:59 +0300

    Delete pozitif_negatif.py

--- pozitif_negatif.py
+++
@@ -1,9 +0,0 @@
-sayi = float(input("Bir sayı girin: "))
-print()
-
-if sayi == 0:
-    print("Giridğiniz sayı 0.")
-elif sayi > 0:
-    print(f"Girdiniz {sayi:g} sayısı pozitif bir sayı.")
-else:
-    print(f"Gridiniz {sayi:g} sayısı negatif bir sayı")
