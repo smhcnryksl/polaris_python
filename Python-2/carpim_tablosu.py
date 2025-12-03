31fc784139ac55237845ec830de918dda1c6c350
smhcnryksl <142000426+smhcnryksl@users.noreply.github.com>
2025-12-03 20:26:47 +0300

    Update carpim_tablosu.py

--- Python-2/carpim_tablosu.py
+++ Python-2/carpim_tablosu.py
@@ -2,6 +2,7 @@ for i in range(9):
     sayi_1 = i + 1
     for n in range(9):
         sayi_2 = n + 1
-        capim = sayi_1 * sayi_2
-        print(f"{n + 1} x {i + 1} = {capim}", end = "  ")
+        carpim = sayi_1 * sayi_2
+        print(f"{n + 1} x {i + 1} = {carpim}", end = "  ")
     print()
+
