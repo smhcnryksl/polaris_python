904c2b6dd6be545fb7c9fe1362262d2cbc12acc3
smhcnryksl <142000426+smhcnryksl@users.noreply.github.com>
2025-12-22 02:02:37 +0300

    Update test cases for password validation

--- Python-3/test_sifre_guvenligi.py
+++ Python-3/test_sifre_guvenligi.py
@@ -4,9 +4,9 @@ from sifre_güvenligi import validate
 def test_validate():
     assert validate("Rakam_Koymadım") == "Güçsüz şifre! Şifreniz en az 1 büyük harf, 1 sayı, ve 1 özel karakter(- _ + # @ $ ! % * ? &) içermelidir."
     assert validate("tumhepsi_kucuk1") == "Güçsüz şifre! Şifreniz en az 1 büyük harf, 1 sayı, ve 1 özel karakter(- _ + # @ $ ! % * ? &) içermelidir."
-    assert validate("busifredeozelyok") == "Güçsüz şifre! Şifreniz en az 1 büyük harf, 1 sayı, ve 1 özel karakter(- _ + # @ $ ! % * ? &) içermelidir."
+    assert validate("busifredeozelyok1") == "Güçsüz şifre! Şifreniz en az 1 büyük harf, 1 sayı, ve 1 özel karakter(- _ + # @ $ ! % * ? &) içermelidir."
     assert validate("bukisa") == "Güçsüz şifre! Şifreniz en az 1 büyük harf, 1 sayı, ve 1 özel karakter(- _ + # @ $ ! % * ? &) içermelidir."
     assert validate("WakeUpNeo") == "Güçsüz şifre! Şifreniz en az 1 büyük harf, 1 sayı, ve 1 özel karakter(- _ + # @ $ ! % * ? &) içermelidir."
     assert validate("   ") == "Eksik şifre"
     assert validate("I_Love_CS50P") == "Şifre onaylandı"
-    assert validate("Stem_Has_No_Limits_1") == "Şifre onaylandı"
+    assert validate("Stem_Has_No_Limits_2018") == "Şifre onaylandı"
