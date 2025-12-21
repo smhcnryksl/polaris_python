ad6f0b0ce29c85e2c707f0ca001511714f5f4ecf
smhcnryksl <142000426+smhcnryksl@users.noreply.github.com>
2025-12-22 01:30:24 +0300

    Refactor data extraction and add phone number cleaning
    Added a function to clean phone numbers and modified the data extraction process to avoid duplicates.

--- Python-3/veri_madencisi_2.py
+++ Python-3/veri_madencisi_2.py
@@ -1,7 +1,28 @@
 import re
 
+
+def tel_temizle(tel):
+    temiz = re.sub("[^\d+]", " ", tel)
+    temiz = temiz.replace("  ", " ")
+    temiz = temiz.strip()
+
+    if temiz[-3] != " ":
+        ilk = temiz[:-2]
+        iki = temiz[-2:]
+        temiz = ilk + " " + iki
+
+    
+    if len(temiz) == 13:
+        return f"+90 {temiz}"
+
+    elif temiz[0] == "0" and len(temiz) >= 10:
+        return f"+90 {temiz[1:]}"
+    else:
+        return temiz
+
+
 veriler = []
-veri = []
+
 
 with open("lvl2_bozuk_veri.txt", encoding="utf-8") as file:
 
@@ -11,12 +32,21 @@ with open("lvl2_bozuk_veri.txt", encoding="utf-8") as file:
     for line in file:
         mail = pattern_mail.findall(line)
         tel = pattern_tel.findall(line)
-        veri = tel + mail
-        veriler.extend(veri)
+        for t in tel:
+            telefon = tel_temizle(t)
+            if telefon not in veriler:
+                veriler.append(telefon)
+            else:
+                continue
 
+        for m in mail:
+            veriler.append(m)
 
 
 with open("lvl2_temiz_rehber.txt", "a") as f:
     for veri in veriler:
         if veri:
-            f.write(f"{veri.strip()}\n")
+            f.write(f"{veri}\n")
+
+
+
