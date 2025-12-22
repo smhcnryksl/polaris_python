7f66ad8e575a8457413a3d71be9cd7fa013a45b7
smhcnryksl <142000426+smhcnryksl@users.noreply.github.com>
2025-12-22 14:41:45 +0300

    Enhance password validation feedback
    Updated validation function to print messages and return boolean values.

--- Python-3/sifre_güvenligi.py
+++ Python-3/sifre_güvenligi.py
@@ -1,20 +1,23 @@
 import re
 
+
 def main():
     sifre = input("Şifre girin: ")
     print(validate(sifre))
 
 
-
 def validate(sifre):
     pattern = re.compile(r"^(?=.*[A-Z])(?=.*\d)(?=.*[-_+@$!%*?&#]).{8,}$")
     if not sifre.strip():
-        return "Eksik şifre"
+        print("Eksik Şifre")
+        return False
     elif re.search(pattern, sifre):
-        return "Şifre onaylandı"
+        print("Şifre Onaylandı")
+        return True
     else:
-        return "Güçsüz şifre! Şifreniz en az 1 büyük harf, 1 sayı, ve 1 özel karakter(- _ + # @ $ ! % * ? &) içermelidir."
-    
+        print("Güçsüz şifre! Şifreniz en az 1 büyük harf, 1 sayı, ve 1 özel karakter(- _ + # @ $ ! % * ? &) içermelidir")
+        return False
+
 
 if __name__ == "__main__":
     main()
