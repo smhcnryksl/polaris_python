3850e3cc9bdd95337db39cdfecb207f0cd0669bd
smhcnryksl <142000426+smhcnryksl@users.noreply.github.com>
2025-12-22 14:24:57 +0300

    Refactor veri_madencisi_2.py for clarity
    Refactor the code for better readability and maintainability.

--- Python-3/veri_madencisi_2.py
+++ Python-3/veri_madencisi_2.py
@@ -1,17 +1,24 @@
 import re
 
+# Telefon numarası ve mail için pattern
+pattern_mail = re.compile(r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}")
+pattern_tel = re.compile(r"\+?\(?\d{0,3} ?\(?\d{3}\)?[ -]\d{3}[ -]?\d{2}[ -]?\d{2}")
+
 veriler = []
 
+# Telefon numarası standartlaştırma işlemi
 def tel_temizle(tel):
     temiz = re.sub("[^\d+]", " ", tel)
     temiz = temiz.replace("  ", " ")
     temiz = temiz.strip()
 
+    # Son 4 karakteri 2'li gruplara ayırma
     if temiz[-3] != " ":
         ilk = temiz[:-2]
         iki = temiz[-2:]
         temiz = ilk + " " + iki
 
+    # Ülke kodu kontrolü ve eklenmesi
     if len(temiz) == 13:
         return f"+90 {temiz}"
 
@@ -21,12 +28,8 @@ def tel_temizle(tel):
         return temiz
 
 
-
+# Metinden verileri çekme
 with open("lvl2_bozuk_veri.txt", encoding="utf-8") as file:
-
-    pattern_mail = re.compile(r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}")
-    pattern_tel = re.compile(r"\+?\(?\d{0,3} ?\(?\d{3}\)?[ -]\d{3}[ -]?\d{2}[ -]?\d{2}")
-
     for line in file:
         mail = pattern_mail.findall(line)
         tel = pattern_tel.findall(line)
@@ -40,7 +43,7 @@ with open("lvl2_bozuk_veri.txt", encoding="utf-8") as file:
         for m in mail:
             veriler.append(m)
 
-
+# Temizlenmiş bilgileri yeni dosyaya yazar
 with open("lvl2_temiz_rehber.txt", "w") as f:
     for veri in veriler:
         if veri:
