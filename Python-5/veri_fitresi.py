4478918eeca32ae49cf20ba57be16b16d7e2a5c3
smhcnryksl <142000426+smhcnryksl@users.noreply.github.com>
2026-03-09 21:20:03 +0300

    Add veri_fitresi.py with filtering logic

---
+++ Python-5/veri_fitresi.py
@@ -0,0 +1,45 @@
+kisiler = [
+    {"ad": "Ahmet", "yas": 25}, {"ad": "Ayşe", "yas": 12}, {"ad": "Mehmet", "yas": 8}, {"ad": "Fatma", "yas": 21}, {"ad": "Ali", "yas": 15},
+    {"ad": "Zeynep", "yas": 18}, {"ad": "Mustafa", "yas": 5}, {"ad": "Emine", "yas": 27}, {"ad": "Hüseyin", "yas": 33}, {"ad": "Hatice", "yas": 10},
+    {"ad": "Murat", "yas": 29}, {"ad": "Özlem", "yas": 14}, {"ad": "Gökhan", "yas": 24}, {"ad": "Selin", "yas": 7}, {"ad": "Burak", "yas": 28},
+    {"ad": "Aslı", "yas": 16}, {"ad": "Eren", "yas": 26}, {"ad": "Derya", "yas": 34}, {"ad": "Mert", "yas": 13}, {"ad": "Merve", "yas": 21},
+    {"ad": "Arda", "yas": 9}, {"ad": "Sude", "yas": 11}, {"ad": "Hakan", "yas": 40}, {"ad": "Melis", "yas": 25}, {"ad": "Yusuf", "yas": 35},
+    {"ad": "Buse", "yas": 17}, {"ad": "Okan", "yas": 29}, {"ad": "Pınar", "yas": 27}, {"ad": "Cem", "yas": 31}, {"ad": "Tuğba", "yas": 24},
+    {"ad": "Kaan", "yas": 6}, {"ad": "Elif", "yas": 30}, {"ad": "Bora", "yas": 37}, {"ad": "Nihal", "yas": 4}, {"ad": "Serkan", "yas": 42},
+    {"ad": "İrem", "yas": 15}, {"ad": "Fatih", "yas": 33}, {"ad": "Gözde", "yas": 26}, {"ad": "Onur", "yas": 12}, {"ad": "Ece", "yas": 21},
+    {"ad": "Deniz", "yas": 31}, {"ad": "Barış", "yas": 16}, {"ad": "Gamze", "yas": 23}, {"ad": "Umut", "yas": 9}, {"ad": "Dilek", "yas": 39},
+    {"ad": "Enes", "yas": 14}, {"ad": "Sibel", "yas": 35}, {"ad": "Tarik", "yas": 30}, {"ad": "Esra", "yas": 28}, {"ad": "Kadir", "yas": 17},
+    {"ad": "Aylin", "yas": 8}, {"ad": "Samed", "yas": 26}, {"ad": "Ceren", "yas": 24}, {"ad": "Volkan", "yas": 32}, {"ad": "Bahar", "yas": 13},
+    {"ad": "Yasin", "yas": 21}, {"ad": "Neslihan", "yas": 36}, {"ad": "Oktay", "yas": 41}, {"ad": "Sinem", "yas": 11}, {"ad": "Tolga", "yas": 34},
+    {"ad": "Asuman", "yas": 52}, {"ad": "Metin", "yas": 15}, {"ad": "Tülay", "yas": 38}, {"ad": "Kenan", "yas": 49}, {"ad": "Nalan", "yas": 10},
+    {"ad": "Sedat", "yas": 27}, {"ad": "Leyla", "yas": 43}, {"ad": "Yavuz", "yas": 6}, {"ad": "Sema", "yas": 26}, {"ad": "Tamer", "yas": 33},
+    {"ad": "Aysel", "yas": 55}, {"ad": "Adnan", "yas": 48}, {"ad": "Filiz", "yas": 12}, {"ad": "Şahin", "yas": 28}, {"ad": "Hande", "yas": 9},
+    {"ad": "Levent", "yas": 36}, {"ad": "Arzu", "yas": 31}, {"ad": "İsmail", "yas": 45}, {"ad": "Hale", "yas": 14}, {"ad": "Taylan", "yas": 27},
+    {"ad": "Ahu", "yas": 7}, {"ad": "Mete", "yas": 23}, {"ad": "Seda", "yas": 32}, {"ad": "Uğur", "yas": 35}, {"ad": "Jale", "yas": 42},
+    {"ad": "Ozan", "yas": 11}, {"ad": "Nur", "yas": 21}, {"ad": "Fırat", "yas": 26}, {"ad": "Eda", "yas": 5}, {"ad": "Emre", "yas": 30},
+    {"ad": "Alper", "yas": 28}, {"ad": "Berna", "yas": 13}, {"ad": "Ender", "yas": 34}, {"ad": "Sevinç", "yas": 40}, {"ad": "Kürşat", "yas": 37},
+    {"ad": "Dila", "yas": 17}, {"ad": "Musa", "yas": 51}, {"ad": "Gül", "yas": 8}, {"ad": "Taner", "yas": 32}, {"ad": "İlknur", "yas": 27},
+    {"ad": "Vedat", "yas": 46}, {"ad": "Mine", "yas": 15}, {"ad": "Cihan", "yas": 28}, {"ad": "Pelin", "yas": 22}, {"ad": "Selçuk", "yas": 39},
+    {"ad": "Aysun", "yas": 10}, {"ad": "Koray", "yas": 25}, {"ad": "Öykü", "yas": 12}, {"ad": "Berkay", "yas": 23}, {"ad": "Didem", "yas": 30},
+    {"ad": "Anıl", "yas": 14}, {"ad": "Sümeyye", "yas": 24}, {"ad": "Ferhat", "yas": 35}, {"ad": "Azra", "yas": 6}, {"ad": "Harun", "yas": 29},
+    {"ad": "Gonca", "yas": 32}, {"ad": "Mesut", "yas": 44}, {"ad": "Belma", "yas": 11}, {"ad": "Ramazan", "yas": 50}, {"ad": "Songül", "yas": 41},
+    {"ad": "Atakan", "yas": 16}, {"ad": "Duygu", "yas": 27}, {"ad": "Sertaç", "yas": 31}, {"ad": "Zuhal", "yas": 36}, {"ad": "Birol", "yas": 43},
+    {"ad": "Gülşen", "yas": 9}, {"ad": "Efe", "yas": 13}, {"ad": "İhsan", "yas": 48}, {"ad": "Tuncay", "yas": 39}, {"ad": "Seher", "yas": 25},
+    {"ad": "Rasim", "yas": 53}, {"ad": "Nevin", "yas": 47}, {"ad": "Saffet", "yas": 55}, {"ad": "Buket", "yas": 14}, {"ad": "Cahit", "yas": 42},
+    {"ad": "Aytaç", "yas": 7}, {"ad": "Suna", "yas": 29}, {"ad": "Erol", "yas": 45}, {"ad": "Melike", "yas": 21}, {"ad": "Sercan", "yas": 12},
+    {"ad": "Zeki", "yas": 54}, {"ad": "Sadi", "yas": 49}, {"ad": "Hülya", "yas": 15}, {"ad": "Bülent", "yas": 46}, {"ad": "Emir", "yas": 8},
+    {"ad": "Nilgün", "yas": 37}, {"ad": "Talat", "yas": 58}, {"ad": "Saniye", "yas": 60}, {"ad": "Yıldız", "yas": 11}, {"ad": "Nuri", "yas": 52}
+]
+
+hedef_kisiler = [kisi for kisi in kisiler if kisi["ad"][0] == "A" or kisi["yas"] > 20]
+
+"""
+def filtrele(*args):
+    return [kisi for kisi in args if kisi["ad"].startswith("A") or kisi["yas"] > 20]
+hedef_kisiler = filtrele(*kisiler)
+"""
+
+print(hedef_kisiler)
+
+
+
